f9f2572d339bf3f1f0dd894ff1947cce1239dec8
JaswalAshish48 <jaswalashish48@gmail.com>
2022-08-30 18:26:50 +0530

    veux: Project-Elixir Bringup
    Signed-off-by: JaswalAshish48 <jaswalashish48@gmail.com>

--- aosp_veux.mk
+++ aosp_veux.mk
@@ -11,15 +11,26 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from veux device
 $(call inherit-product, device/xiaomi/veux/device.mk)
 
-# Inherit some common Evolution X stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
-TARGET_USES_AOSP_RECOVERY := true
+# Inherit some common Elixir stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
+
+# Official
+ELIXIR_BUILD_TYPE := OFFICIAL
+ELIXIR_MAINTAINER := JaswalAshish
+
+# Project-Elixir Stuff
+IS_PHONE := true
+TARGET_USES_AOSP_RECOVERY := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_INCLUDE_STOCK_ARCORE := false
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
-PRODUCT_NAME := evolution_veux
+PRODUCT_NAME := aosp_veux
 PRODUCT_DEVICE := veux
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
